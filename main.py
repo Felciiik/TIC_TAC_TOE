08d224f68e230808df23f52ea23f3979a538c1d7
Matěj Babčan <babcan.matej@seznam.cz>
2022-09-05 23:18:25 +0200

    Dynamic board size. Player choose board size from 3 to 10.

--- main.py
+++ main.py
@@ -7,14 +7,48 @@ class TicTacToe:
     def __init__(self, empty_sign="-"):
         self.board = []
         self.empty_sign = empty_sign
+        self.board_size = self.choose_size_of_board()
+
+    def choose_size_of_board(self):
+        while True:
+            size = input(textwrap.dedent("""
+                                    --- \n
+                                    Choose size of game board (Enter number). \n
+                                    (Minimal size of board is 3 and maximal size of board is 10) \n
+                                    (size of board will be entered number x entered number e.g. 3x3) \n
+                                    --- \n
+                                        """))
+            try:
+                size = int(size)
+            except:
+                print(textwrap.dedent("""
+                                --- \n
+                                You entered wrong symbol. \n
+                                Enter only number. \n
+                                --- \n
+                                     """))
+                continue
+
+            if 3 <= size <= 10:
+                break
+            else:
+                print(textwrap.dedent("""
+                                --- \n
+                                Minimal size of board is 3 and maximal size of board is 10. \n
+                                Enter only number. \n
+                                --- \n
+                                     """))
+                continue
+        return size
+
 
 #Block bellow is not my code
 ###############################################################
 
     def create_board(self):
-        for row in range(3):
+        for row in range(self.board_size):
             row_temp = []
-            for column in range(3):
+            for column in range(self.board_size):
                 row_temp.append(self.empty_sign)
             self.board.append(row_temp)
 
@@ -32,6 +66,60 @@ class TicTacToe:
             else:
                 continue
         return player
+
+    def choose_field_with_check(self, player):
+        while True:
+            player_row_col = input(textwrap.dedent("""
+                                --- \n
+                                Enter row, column numbers to occupy chosen field. \n
+                                Required format is row and column number separated by comma.  \n
+                                For example '2, 3'. \n
+                                --- \n
+                                                    """))
+            player_row_col = player_row_col.replace(" ", "").split(",")
+            if len(player_row_col) != 2:
+                print(textwrap.dedent("""
+                                --- \n
+                                You entered row and column in wrong format. \n
+                                There was probably too many commas (",") \n
+                                Read guidelines again and enter row adn column again. \n
+                                --- \n
+                                      """))
+                continue
+            try:
+                row, col = list(map(int, (player_row_col)))
+            except ValueError:
+                print(textwrap.dedent("""
+                                --- \n
+                                You entered row and column in wrong format. \n
+                                At least one symbol around comma is not number or there are some unwanted symbols.\n
+                                Read guidelines again and enter row adn column again. \n
+                                --- \n
+                                    """))
+                continue
+            if row < 1 or col < 1:
+                print(textwrap.dedent("""
+                                --- \n
+                                You entered row and column in wrong format. \n
+                                At least one number is lower than 1 (There can not be negative rows\cols). \n
+                                Read guidelines again and enter row adn column again. \n
+                                --- \n
+                                      """))
+                continue
+            row, col = row - 1, col - 1  # Zeroindexing
+
+            if self.board[row][col] == self.empty_sign:
+                self.set_spot_value(row, col, player)
+                break
+            else:
+                print(textwrap.dedent("""
+                                    --- \n
+                                    You entered coordinates of occupied field. \n
+                                    Try it again. \n
+                                    --- \n
+                                      """))
+                continue
+
 
     def set_spot_value(self, row, column, player):
             self.board[row][column] = player
@@ -76,7 +164,7 @@ class TicTacToe:
             if size_of_board == 3:
                 iterator = [2]
             else:
-                iterator = range(size_of_board, 2, -1)
+                iterator = range(size_of_board-1, 2, -1)
             return iterator
 
         ## From right side
@@ -105,6 +193,9 @@ class TicTacToe:
             print("-----" * len(row))
 
     def start_game(self):
+
+        size = self.board_size
+
         self.create_board()
 
         player = self.first_player_choose_sign()
@@ -114,59 +205,7 @@ class TicTacToe:
 
             self.show_board()
 
-            while True:
-                player_row_col = input(textwrap.dedent("""
-                                    --- \n
-                                    Enter row, column numbers to occupy chosen field. \n
-                                    Required format is row and column number separated by comma.  \n
-                                    For example '2, 3'. \n
-                                    --- \n
-                                                        """))
-                player_row_col = player_row_col.replace(" ", "").split(",")
-                if len(player_row_col) != 2:
-                    print(textwrap.dedent("""
-                                    --- \n
-                                    You entered row and column in wrong format. \n
-                                    There was probably too many commas (",") \n
-                                    Read guidelines again and enter row adn column again. \n
-                                    --- \n
-                                          """))
-                    continue
-                try:
-                    row, col = list(map(int, (player_row_col)))
-                except ValueError:
-                    print(textwrap.dedent("""
-                                    --- \n
-                                    You entered row and column in wrong format. \n
-                                    At least one symbol around comma is not number or there are some unwanted symbols.\n
-                                    Read guidelines again and enter row adn column again. \n
-                                    --- \n
-                                        """))
-                    continue
-                if row < 1 or col < 1:
-                    print(textwrap.dedent("""
-                                    --- \n
-                                    You entered row and column in wrong format. \n
-                                    At least one sumber is lower than 1 (There can not be negative rows\cols). \n
-                                    Read guidelines again and enter row adn column again. \n
-                                    --- \n
-                                          """))
-                    continue
-                row, col = row - 1, col - 1 #Zeroindexing
-                break
-
-            if self.board[row][col] == self.empty_sign:
-                self.set_spot_value(row, col, player)
-            else:
-                print(textwrap.dedent("""
-                                    --- \n
-                                    You entered coordinates of occupied field. \n
-                                    Try it again. \n
-                                    --- \n
-                                      """))
-                continue
-
-
+            self.choose_field_with_check(player)
 
             if self.does_anyone_win():
                 print(f"Player {player} wins the game!")
