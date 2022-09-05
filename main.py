df709c739e5cba0dae923fe60534c3ed7ecc2349
Matěj Babčan <babcan.matej@seznam.cz>
2022-09-05 21:08:31 +0200

    Player now can not overwrite opponents field.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import textwrap
 import itertools
 
 class TicTacToe:
@@ -21,10 +22,6 @@ class TicTacToe:
 ################################################################
 
     def set_spot_value(self, row, column, player):
-        if self.board[row][column] == self.empty_sign:
-            self.board[row][column] = player
-        else:
-            # todo Hráč by měl dostat nový pokus.
             self.board[row][column] = player
 
     def does_anyone_win(self):
@@ -106,47 +103,58 @@ class TicTacToe:
             self.show_board()
 
             while True:
-                player_row_col = input("""
---- \n
-Enter row, column numbers to occupy chosen field. \n
-Required format is row and column number separated by comma.  \n
-For example '2, 3'. \n
---- \n
-                                       """)
+                player_row_col = input(textwrap.dedent("""
+                                    --- \n
+                                    Enter row, column numbers to occupy chosen field. \n
+                                    Required format is row and column number separated by comma.  \n
+                                    For example '2, 3'. \n
+                                    --- \n
+                                                        """))
                 player_row_col = player_row_col.replace(" ", "").split(",")
                 if len(player_row_col) != 2:
-                    print("""
---- \n
-You entered row and column in wrong format. \n
-There was probably too many commas (",") \n
-Read guidelines again and enter row adn column again. \n
---- \n
-                         """)
+                    print(textwrap.dedent("""
+                                    --- \n
+                                    You entered row and column in wrong format. \n
+                                    There was probably too many commas (",") \n
+                                    Read guidelines again and enter row adn column again. \n
+                                    --- \n
+                                          """))
                     continue
                 try:
                     row, col = list(map(int, (player_row_col)))
                 except ValueError:
-                    print("""
---- \n
-You entered row and column in wrong format. \n
-At least one symbol around comma is not number or there are some unwanted symbols.\n
-Read guidelines again and enter row adn column again. \n
---- \n
-                          """)
+                    print(textwrap.dedent("""
+                                    --- \n
+                                    You entered row and column in wrong format. \n
+                                    At least one symbol around comma is not number or there are some unwanted symbols.\n
+                                    Read guidelines again and enter row adn column again. \n
+                                    --- \n
+                                        """))
                     continue
                 if row < 1 or col < 1:
-                    print("""
---- \n
-You entered row and column in wrong format. \n
-At least one sumber is lower than 1 (There can not be negative rows\cols). \n
-Read guidelines again and enter row adn column again. \n
---- \n
-                          """)
+                    print(textwrap.dedent("""
+                                    --- \n
+                                    You entered row and column in wrong format. \n
+                                    At least one sumber is lower than 1 (There can not be negative rows\cols). \n
+                                    Read guidelines again and enter row adn column again. \n
+                                    --- \n
+                                          """))
                     continue
                 row, col = row - 1, col - 1 #Zeroindexing
                 break
 
-            self.set_spot_value(row, col, player)
+            if self.board[row][col] == self.empty_sign:
+                self.set_spot_value(row, col, player)
+            else:
+                print(textwrap.dedent("""
+                                    --- \n
+                                    You entered coordinates of occupied field. \n
+                                    Try it again. \n
+                                    --- \n
+                                      """))
+                continue
+
+
 
             if self.does_anyone_win():
                 print(f"Player {player} wins the game!")
